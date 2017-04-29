c89ae1c97fe5290c7a3fe48028947a60c0ce2b99
javier <perezrubio.javier@gmail.com>
2017-04-29 10:56:44 +0100

    PEP8

---
+++ lcd_service.py
@@ -0,0 +1,15 @@
+from lcd import LCD
+import sys
+
+if __name__ == "__main__":
+    if (len(sys.argv) != 2) or (sys.argv[1] not in ('0', '1')):
+        print "Usage: sudo python LCD command where:"
+        print "command = 1  on"
+        print "command = 0 off"
+        quit()
+
+    lcd = LCD()
+    if sys.argv[1] == '0':
+        lcd._cleanUp()
+    else:
+        lcd.blight(1)
