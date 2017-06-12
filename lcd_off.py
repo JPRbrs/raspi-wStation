079bf938fb11794ddd6e19baddd08eae2cbb7f20
javier <elmasrallao@hotmail.com>
2017-06-12 23:32:52 +0100

    Add lcd_off

---
+++ lcd_off.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from lcd import LCD
+
+
+def main():
+    try:
+        lcd = LCD()
+        lcd.blight(0)
+    except Exception, e:
+        print e
+        lcd._clean_up()
+        quit()
+    except KeyboardInterrupt:
+        print 'User interrupted'
+        lcd._clean_up()
+        quit()
+
+if __name__ == '__main__':
+    main()
+
