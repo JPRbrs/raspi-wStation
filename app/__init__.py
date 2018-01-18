ee2061ee3815b89b2077a332184a95b41271c1c4
Javier# <javier@tuware.com>
2018-01-18 23:38:04 +0000

    Add flask-alembic

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ DB_PATH = "/home/javier/projects/raspi-wStation/home.db"
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////{}'.format(DB_PATH)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 import views
