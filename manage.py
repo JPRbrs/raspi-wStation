2cc951c24badb7cb38d7135e97018a492f0ed820
Javier# <javier@tuware.com>
2018-01-19 23:14:51 +0000

    clean up manage.py

--- manage.py
+++ manage.py
@@ -1,17 +1,8 @@
-# from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app import app
+from app import app, db
 
-DB_PATH = "/home/javier/projects/raspi-wStation/home.db"
-
-# app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////{}'.format(DB_PATH)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 manager = Manager(app)
