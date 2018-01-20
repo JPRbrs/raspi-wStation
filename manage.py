673e753060e215381548b76fe197d0d5199b38cb
Javier <elmasrallao@hotmail.com>
2018-01-20 00:31:01 +0000

    try to add commands

--- manage.py
+++ manage.py
@@ -1,7 +1,18 @@
-from flask_script import Manager
+from datetime import datetime
+from time import sleep
+
+from flask import Flask
 from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+import weather
 
 from app import app, db
+from app.dht import get_hum_and_temp
+from app.dbi import save_instant
+from lcd import LCD
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
 
@@ -14,5 +25,34 @@ def hello():
     print "hello"
 
 
-if __name__ == "__main__":
-    manager.run()
+@manager.command
+def save_instant_into_db():
+    save_instant()
+
+
+@manager.command
+def cron_lcd():
+    temp, hum = get_hum_and_temp()
+    time = datetime.now().strftime('%H:%M')
+    weather_dict = weather.get_weather()
+    line1 = "{0:0.1f} C and {1:0.1f}%".format(temp, hum)
+    line2 = time + " Feels %d" % (
+        int(weather_dict['out_feel'])
+    )
+    try:
+        lcd = LCD()
+        sleep(0.5)
+        lcd.send_text(line1, 1)
+        lcd.send_text(line2, 2)
+    except Exception, e:
+        print e
+        lcd._clean_up()
+        quit()
+    except KeyboardInterrupt:
+        print 'User interrupted'
+        lcd._clean_up()
+        quit()
+
+
+#if __name__ == "__main__":
+#    manager.run()
