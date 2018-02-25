9f7124f032c796d5c192671b45366a0fe366e920
Javier# <javier@tuware.com>
2018-02-25 12:27:14 +0000

    Get more info in weather module

--- weather.py
+++ weather.py
@@ -8,23 +8,48 @@ import pywapi
 
 
 def get_weather():
-    # Returns a dict with two keys: out_feel and out_conditions
-    retval = {}
+    """
+    Returns a dict with the weather info to stored in the database
+    """
+    weather = pywapi.get_weather_from_weather_com('BS16:4:UK')
+    curr_cond = weather['current_conditions']
+
+    ret_dict = {
+        'feels_like': int(curr_cond['feels_like']),
+        'out_temperature': int(curr_cond['temperature']),
+        'out_humidity': int(curr_cond['humidity']),
+        'uv_index': int(curr_cond['uv']['index']),
+        'wind_direction': curr_cond['wind']['text'],
+        'wind_speed': int(curr_cond['wind']['speed']),
+        'out_conditions': curr_cond['text'].lower(),
+    }
+
+    return ret_dict
+
+
+def get_forecast(days_from_today):
+    """
+    Returns tomorrows weather conditions a numbers of days after today
+    equal to the passed parameter
+    """
+    if days_from_today not in (1, 2, 3):
+        raise ValueError('Days_From_Today must be 1, 2 or 3')
 
     weather = pywapi.get_weather_from_weather_com('BS16:4:UK')
-    retval['out_feel'] = weather['current_conditions']['feels_like']
-    retval['out_conditions'] = weather['current_conditions']['text'].lower()
+    forecast = weather['forecasts'][days_from_today]
 
-    return retval
-
-
-def main():
-    weather = get_weather()
-
-    print "%s and feels %d outside" % (
-        weather['out_conditions'],
-        int(weather['out_feel']))
-
-
-if __name__ == '__main__':
-    main()
+    ret_dict = {
+        'date': forecast['date'],
+        'high': forecast['high'],
+        'low': forecast['low'],
+        'sunrise': forecast['sunrise'],
+        'sunset': forecast['sunset'],
+        'day_text': forecast['day']['text'],
+        'chance_precip': forecast['day']['chance_precip'],
+        'day_wind_gust': forecast['day']['wind']['gust'],
+        'night_text': forecast['night']['text'],
+        'night_humidity': forecast['night']['humidity'],
+        'night_precip': forecast['night']['chance_precip'],
+        'night_wind_gust': forecast['night']['wind']['gust']
+    }
+    return ret_dict
